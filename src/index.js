68f5ebe3d534e5ae866ff8746c6abb127aa5c38b
AMJeffries <millyjeffries_95@hotmail.com>
2021-11-29 13:56:29 -0900

    Weather Forecast JS Update

--- src/index.js
+++ src/index.js
@@ -60,6 +60,37 @@ farenheitLink.addEventListener("click", convertToFahrenheit);
 let celsiusLink = document.querySelector("#tempC");
 celsiusLink.addEventListener("click", convertToCelsius);
 
+//Weather Forecast
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> -7° </span>
+          <span class="weather-forecast-temperature-min"> -8° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast();
+
 //Search for city and display
 function displayWeather(response) {
   console.log(response.data);
